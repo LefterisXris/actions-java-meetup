d0fefada04deb231e569e285e6b558577338e053
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2024-09-26 19:45:32 +0300

    init commit

---
+++ src/test/java/org/java/meetup/skg/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.meetup.skg.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
